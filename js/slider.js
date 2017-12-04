efe97113259570fe3138a265f4ce977de45cce9d
Ron Grass <GrassRo@brc.gbl>
2017-12-03 18:42:11 -0600

    Iniitial commit Beginning to build out hmtl

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var slideout = new Slideout({
+    'panel': document.getElementById('main'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+
+  document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+    slideout.toggle();
+  });
