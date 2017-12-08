796944c272291f1cad2474eb68aea9694b288b20
Ron Grass <GrassRo@brc.gbl>
2017-12-08 14:31:48 -0600

    Created mb.js to hold the critical app javascript Began migration of Udacity map code into ap Added map styles from Udacity into css file

---
+++ js/mb.js
@@ -0,0 +1,44 @@
+var city = "Milwaukee, WI";
+
+var breweries = [
+    {
+        "brewery_name": "Name 1",
+        "placeid": "PlaceID 1",
+        "beer_id": "BeerID 1",
+        "visible": true
+    },
+    {
+        "brewery_name": "Name 2",
+        "placeid": "PlaceID 2",
+        "beer_id": "BeerID 2",
+        "visible": true
+    },
+    {
+        "brewery_name": "Name 3",
+        "placeid": "PlaceID 3",
+        "beer_id": "BeerID 3",
+        "visible": true
+    },
+    {
+        "brewery_name": "Name 4",
+        "placeid": "PlaceID 4",
+        "beer_id": "BeerID 4",
+        "visible": true
+    },
+    {
+        "brewery_name": "Name 5",
+        "placeid": "PlaceID 5",
+        "beer_id": "BeerID 5",
+        "visible": true
+    }
+];
+
+
+
+
+
+// Load required Google Map APIs
+<script async defer
+    src=
+    "https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=AIzaSyAdkv8cWmJKMSYSpCy7X5MLr2a1AYjh4Us&v=3&callback=initMap">
+</script>
